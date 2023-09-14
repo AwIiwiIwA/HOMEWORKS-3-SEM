6532270a8fbd612e85d4e802d4fe6f8823592160
Alehandro Krava <115242003+AwIiwiIwA@users.noreply.github.com>
2023-09-14 13:59:00 +0300

    HW 1-1

---
+++ Matrix.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Matrix
+{
+    public int[,] Data { get; }
+    public int Rows { get; }
+    public int Columns { get; }
+
+    public Matrix(int rows, int columns)
+    {
+        Rows = rows;
+        Columns = columns;
+        Data = new int[rows, columns];
+    }
+
+    public static Matrix Multiply(Matrix A, Matrix B)
+    {
+        if (A.Columns != B.Rows)
+            throw new InvalidOperationException("The number of columns in matrix A must be equal to the number of rows in matrix B.");
+
+        Matrix result = new Matrix(A.Rows, B.Columns);
+
+        for (int i = 0; i < A.Rows; i++)
+        {
+            for (int j = 0; j < B.Columns; j++)
+            {
+                int sum = 0;
+                for (int k = 0; k < A.Columns; k++)
+                {
+                    sum += A.Data[i, k] * B.Data[k, j];
+                }
+                result.Data[i, j] = sum;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public static class Utils
+{
+    public static Matrix GenerateRandomMatrix(int rows, int columns, int maxValue)
+    {
+        Random rand = new Random();
+        Matrix matrix = new Matrix(rows, columns);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                matrix.Data[i, j] = rand.Next(maxValue);
+            }
+        }
+
+        return matrix;
+    }
+
+    public static bool AreMatricesEqual(Matrix A, Matrix B)
+    {
+        if (A.Rows != B.Rows || A.Columns != B.Columns)
+            return false;
+
+        for (int i = 0; i < A.Rows; i++)
+        {
+            for (int j = 0; j < A.Columns; j++)
+            {
+                if (A.Data[i, j] != B.Data[i, j])
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
